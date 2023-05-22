7841010119f3c1335623a69d8af73e08092afcf2
Michaella Rodriguez <micha.ella@hotmail.com>
2023-05-22 18:59:59 -0500

    rendering list items, styling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https:www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-f5543-default-rtdb.firebaseio.com/"
@@ -13,11 +13,18 @@ const inputEl = document.getElementById("input-el")
 const addButtonEl = document.getElementById("add-button-el")
 const listEl = document.getElementById("list-el")
 
+onValue(listInDB, (snapshot) => {
+    let itemsArray = Object.values(snapshot.val())
+    clearListEl()
+    for (let i = 0; i < itemsArray.length; i++) {
+        addItemToList(itemsArray[i])
+    }
+})
+
 addButtonEl.addEventListener("click", () => {
     let inputVal = inputEl.value
     push(listInDB, inputVal)
     clearInputField()
-    addItemToList(inputVal)
 })
 
 function clearInputField() {
@@ -26,4 +33,8 @@ function clearInputField() {
 
 function addItemToList(itemValue) {
     listEl.innerHTML += `<li class="list-item">${itemValue}</li>`
+}
+
+function clearListEl() {
+    listEl.innerHTML = ""
 }
